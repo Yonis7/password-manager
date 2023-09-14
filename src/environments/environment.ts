9f9fed366186b9c4afa113f0aa1b8daf38bd492e
Yonis7 <yonisaden7@gmail.com>
2023-09-14 03:48:44 -0400

    Navbar and add site front end complete

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'password-manager-d84ce',
+    appId: '1:492195053995:web:435050d94b0a0b47af92ee',
+    databaseURL: 'https://password-manager-d84ce-default-rtdb.firebaseio.com',
+    storageBucket: 'password-manager-d84ce.appspot.com',
+    apiKey: 'AIzaSyDgQbEQwOnyWThZ5GoONDX41-gZKKksPBE',
+    authDomain: 'password-manager-d84ce.firebaseapp.com',
+    messagingSenderId: '492195053995',
+  },
+  prodiction: true
+};
