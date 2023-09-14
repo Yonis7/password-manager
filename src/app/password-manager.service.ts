d856e2dacfc0c29e1fbf4f6fa7957456cf2dfb90
Yonis7 <yonisaden7@gmail.com>
2023-09-14 13:07:44 -0400

    Completed save query

--- src/app/password-manager.service.ts
+++ src/app/password-manager.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
+import { addDoc } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PasswordManagerService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) { }
+
+  addSite(data: object) {
+    const dbInstance = collection(this.firestore, 'sites');
+    return addDoc(dbInstance, data)
+  }
 }
